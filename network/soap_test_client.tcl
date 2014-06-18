f4fd9576a1b85b5da7d59522063af590d2c3883a
Ricardo Victor Guiman II <rvguiman@gmail.com>
2014-06-18 17:21:33 +0800

    Create soap_test_client.tcl

---
+++ network/soap_test_client.tcl
@@ -0,0 +1,16 @@
+#!/bin/sh
+# next line restarts using tclsh \
+exec /APP1/Tcl/Linux-x86_64/ActiveTcl8.5.9.2/bin/tclsh "$0" ${1+"$@"}
+
+#--------------------------------------------------------------
+# what: simple client test for ruby soapclient at ruby repo
+# author: ricardo
+#--------------------------------------------------------------
+
+# change to package your path
+# lappend ::auto_path /your_path/Tcl/lib/
+
+package require SOAP
+
+SOAP::create sum -uri "urn:ruby:mathcalc" -proxy "http://127.0.0.1:9900" -params { add num1 num2 }
+puts [::sum 10 30]
