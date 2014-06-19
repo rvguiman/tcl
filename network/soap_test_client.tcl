4e38fc629a9a0e60dec8b48f3eb8babaf5eb36a2
Ricardo Victor Guiman II <rvguiman@gmail.com>
2014-06-19 11:13:36 +0800

    Update soap_test_client.tcl

--- network/soap_test_client.tcl
+++ network/soap_test_client.tcl
@@ -12,9 +12,9 @@ exec /APP1/Tcl/Linux-x86_64/ActiveTcl8.5.9.2/bin/tclsh "$0" ${1+"$@"}
 
 package require SOAP
 
-SOAP::create sum -uri "urn:ruby:mathcalc" -proxy "http://127.0.0.1:9900" -params { num1 int num2 int }
+SOAP::create sum -uri "urn:ruby:mathcalc" -proxy "http://127.0.0.1:9900" -params { a integer b integer }
 puts [::sum 10 30]
 
-SOAP::create dif -uri "urn:ruby:mathcalc" -proxy "http://127.0.0.1:9900" -params { num1 int num2 int }
-puts [::dif 10 30]
+SOAP::create dif -uri "urn:ruby:mathcalc" -proxy "http://127.0.0.1:9900" -params { a integer b integer }
+puts [::dif 50 30]
 
